8cf284f5f404aaf0dacb81a21160fba1a59c7e9b
Jessica A <161026755+jessislearning@users.noreply.github.com>
2024-08-05 22:12:54 +0800

    built "transfer" method

--- budget.py
+++ budget.py
@@ -1,33 +1,33 @@
 class Category:
-    def __init__(self):
+    def __init__(self, category_name):
+        self.category_name = category_name
         self.ledger=[]
         self.balance=0.0
-
-    def deposit(self, amount, description=""):
+        
+    def deposit (self, amount, description=""):
         self.ledger.append({'amount': amount, 'description': description})
         self.balance+=amount
 
-    def widthdraw(self, amount, description=""):        
-        if amount > self.balance:
-            self.ledger.append({'amount': amount*-1, 'description': description})
+    def withdraw (self, amount, description=""):        
+        if amount <= self.balance:
+            self.ledger.append({'amount': -1*amount, 'description': description})
             self.balance+=amount
             return True
-        elif amount <= self.balance:
-            balance = self.balance
+        elif amount > self.balance:
             return False
     
     def get_balance(self):
         return self.balance
     
-    def transfer(self):
-        pass
+    def transfer(self, amount, transfer_cat):
+        self.withdraw(amount, f"Transfer to {transfer_cat.category_name}")
+        transfer_cat.deposit(amount, f"Transfer from {self.category_name}")
 
     def check_funds(self, amount):
-        if amount > self.balance:
-            return False
         if amount <= self.balance:
             return True
-
+        if amount > self.balance:
+            return False
 
 
 def create_spend_chart(categories):
